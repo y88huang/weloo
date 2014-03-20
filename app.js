cac6a23308a90d25a9d75d26946b0443b05d16b4
y88huang <kenkaka.hyd@gmail.com>
2014-03-20 16:53:19 -0400

    add token

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ var verbose = require('debug')('webot-example:verbose');
 var app = express();
 
 // 实际使用时，这里填写你在微信公共平台后台填写的 token
-var wx_token = process.env.WX_TOKEN || 'keyboardcat123';
-var wx_token2 = process.env.WX_TOKEN_2 || 'weixinToken2';
-
+// var wx_token = process.env.WX_TOKEN || 'keyboardcat123';
+// var wx_token2 = process.env.WX_TOKEN_2 || 'weixinToken2';
+var wx_token = process.env.WX_TOKEN || 'wewaterloo2014';
+var wx_token2 = process.env.WX_TOKEN_2 || 'wewaterloo2014';
 // 建立多个实例，并监听到不同 path ，
 var webot2 = new webot.Webot();
 
