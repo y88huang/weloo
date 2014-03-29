2041d3d4b282d8cb74ce69e0af9f8b400f30031a
Elton Gao <gyfelton@gmail.com>
2014-03-29 14:27:06 -0400

    create uwapi wrapper for refactor create localized text helper in utils.js for language switch

--- rules/uwaterloo/terms/exam_schedule.js
+++ rules/uwaterloo/terms/exam_schedule.js
@@ -1,43 +1,47 @@
-var httpsync = require('httpsync');
+var uwapi = require('../../../utils/uwapi');
+var utils = require('../../../utils/utils');
 
 module.exports = function(webot) {
   // Exam Command Rule
-  webot.waitRule('wait_course', function(info) {
+  webot.waitRule('wait_course', function(info, next) {
     try {
       var subject = info.text.match(/\D+/)[0];
       var catalogNum = info.text.match(/\d+/)[0];  
     } catch(err) {
-      return null;
+      //input format is wrong
+      next(null, utils.localizedText(webot, 
+        {
+          'en_us' : 'Invalid format, please enter in this format: cs486',
+          'zh_cn' : "格式不正确，请仿造以下例子：cs488"
+        });
     }
-    var url = 'http://api.uwaterloo.ca/v2/courses/'+subject+'/'+catalogNum+'/examschedule.json?key=3b959fae56b87fa8e0c5d416525d3b4e';
-    req = httpsync.get(url);
-    var res = req.end();
-    var data = JSON.parse(res['data'].toString('utf-8'));
-    if (data['meta']['message'] == 'No data returned') {
-      info.rewait();
-      return 'Oops! No match!\nPlease try again.';
-    }
-    else {
-      data = data['data'];
-      var course = data['course'];
-      var descrip = data['sections'];
-      var result = new Array();
-      for (var i = 0; i < descrip.length; i++) {
-        var section = descrip[i]['section'];
-        var day = descrip[i]['day'];
-        var date = descrip[i]['date'];
-        var start = descrip[i]['start_time'];
-        var end = descrip[i]['end_time'];
-        var location = descrip[i]['location'];
-        var exam = 'Section: '+section+', Location: '+location;
-        result.push(exam);
-      }
-      var reply = {
-        title: course+'\t'+date+' '+day+'\n'+start+' - '+end, 
-        description: result.join('\n')
+    uwapi.getjson('courses/'+subject+'/'+catalogNum+'/examschedule', function(data) {
+      //No Course found
+      if (data['meta']['message'] == 'No data returned') {
+        info.rewait();
+        next(null, 'Oops! No match!\nPlease try again.');
+      } else {
+        data = data['data'];
+        var course = data['course'];
+        var descrip = data['sections'];
+        var result = new Array();
+        for (var i = 0; i < descrip.length; i++) {
+          var section = descrip[i]['section'];
+          var day = descrip[i]['day'];
+          var date = descrip[i]['date'];
+          var start = descrip[i]['start_time'];
+          var end = descrip[i]['end_time'];
+          var location = descrip[i]['location'];
+          var exam = 'Section: '+section+', Location: '+location;
+          result.push(exam);
+        }
+        var reply = {
+          title: course+'\t'+date+' '+day+'\n'+start+' - '+end, 
+          description: result.join('\n')
+        };
+        next(null, reply);
       };
-      return reply;
-    }
+    });
   });
 
   webot.set('exam schedule', {
@@ -45,11 +49,12 @@ module.exports = function(webot) {
     pattern: /^(exam)$/i,
     handler: function(info) {
       info.wait('wait_course');
-      if (webot.config.lang === 'en_us') {
-      	return 'Please enter your course subject and number. (e.g. CS486)';
-  	  } else {
-  	  	return "请输入你的Course Number, 比如 cs116：";
-  	  }
+      return utils.localizedText(webot, 
+        {
+          'en_us' : 'Please enter your course subject and number. (e.g. CS486)',
+          'zh_cn' : "请输入你的Course Number, 比如 cs116："
+        }
+      );
     }
   });
 };
